e3c33d086e838ab1fd037c6ca543679e7dc7bb7d
Luidi Matheus Silva De Oliveira <120000610@portodoacu.com.br>
2020-03-06 13:56:28 -0300

    Aula 3

---
+++ 3 - GlideSystem.js
@@ -0,0 +1,17 @@
+//Registrar um log
+gs.log('Test', 'marks_log');
+
+//Registrar um error
+gs.error('Test');
+
+//Registrar um aviso
+gs.warn('Test');
+
+//Primeiro dia do último mês
+gs.print(gs.beginningOfLastMonth());
+
+//Gerar um ID
+gs.print(gs.generateGUID());
+
+//Pegar uma menssagem salva
+gs.print(gs.getMessage('Sum');
